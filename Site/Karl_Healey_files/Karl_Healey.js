ff5f8aafb4ee8aa5bd900d7cfc545f48045c5618
karlhealey <mkarlhealey@me.com>
2012-04-23 14:05:19 -0400

    updating site

--- Site/Karl_Healey_files/Karl_Healey.js
+++ Site/Karl_Healey_files/Karl_Healey.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20120330
+// Created by iWeb 3.0.4 local-build-20120423
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
